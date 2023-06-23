3388eabf4377602c4c5afedaf87f77be3ea0d44d
CedricFriscia <cedricfriscia@gmail.com>
2023-06-23 14:19:40 +0200

    add party video

--- src/Pages/Project/index.jsx
+++ src/Pages/Project/index.jsx
@@ -26,6 +26,28 @@ const Project = () => {
             </a>
           </div>
         </div>
+        <div className="card">
+          <div className="image-party"></div>
+          <div className="content">
+            <a href="#">
+              <span className="title">Party</span>
+            </a>
+
+            <p className="desc">
+              Une application qui permet de créer des événements et d'inviter
+              vos proches. Video du projet Apothéose.
+            </p>
+
+            <a
+              href="https://www.youtube.com/watch?v=5RN9SH2qkLE"
+              className="action"
+              target="_blank"
+            >
+              Show the video
+              <span aria-hidden="true">...</span>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
